f45ad7d5b85f0d3eb02704f87dc76dbc874effe3
scott <scott@zamplus.com>
2015-05-20 15:14:48 +0800

    create git structure

---
+++ src/main/java/com/scott/dev/base/BaseObject.java
@@ -0,0 +1,9 @@
+package com.scott.dev.base;
+
+import java.io.Serializable;
+
+public abstract class BaseObject implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+}
